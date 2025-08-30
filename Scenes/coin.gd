7650a832494e3cbb91cfa4af597191b1413d2024
epicCoolDog <epiccooldog@techie.com>
2025-08-30 19:18:28 -0400

    Coin spawning is broken

--- Scenes/coin.gd
+++ Scenes/coin.gd
@@ -1,1 +1,11 @@
 extends Area2D
+
+func _on_body_entered(body):
+	print("Body entered")
+	if body.is_in_group("player"):
+		print("Chest opened")
+		
+		body.coins += 1
+		print("Coin detected")
+		
+		queue_free()
